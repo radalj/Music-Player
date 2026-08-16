09f083d115ae7b216f02d7c49d743d02e53bf093
Hosna84 <hosnashahheidari1384@gmail.com>
2026-08-16 11:28:21 +0330

    test(frontend): keep Playwright specs out of the Jest suite

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -8,6 +8,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/e2e/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
